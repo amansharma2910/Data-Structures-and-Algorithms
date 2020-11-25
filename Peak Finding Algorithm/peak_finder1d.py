c826bb8b797ac58d3a8b1e0ba92b51eb9b8fe281
amansharma2910 <amansharma2910@gmail.com>
2020-11-25 21:52:53 +0530

    Made changes to PFA-1D

--- Peak Finding Algorithm/peak_finder1d.py
+++ Peak Finding Algorithm/peak_finder1d.py
@@ -5,6 +5,7 @@ def one_D_peak_finder(arr):
             return arr[n]
         else:
             return one_D_peak_finder(arr[n+1:]) or one_D_peak_finder(arr[:n])
+        
 
 if __name__ == "__main__":
     arr = [10, 20, 25, 2, 23, 90, 67, 24, 65, 74, 87, 12, 98, 23, 67, 83, 86]
---
+++ Peak Finding Algorithm/peak_finder2d.py
@@ -0,0 +1,2 @@
+def two_D_peak_finding(mat):
+    pass
